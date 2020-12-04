15f39c8fabfcb6fdefa423bf1e6ed21e7f375b27
MrSkl1f <sklifden@mail.ru>
2020-12-04 14:27:57 +0300

    Fix

--- Lab2/prog4.c
+++ Lab2/prog4.c
@@ -3,37 +3,10 @@
 #include <unistd.h>
 #include <sys/types.h> 
 #include <sys/wait.h>
-#include <string.h>
-
-void reverse(char s[])
-{
-    int i, j;
-    char c;
-
-    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) 
-    {
-        c = s[i];
-        s[i] = s[j];
-        s[j] = c;
-    }
-}
-
-void convert_to_str(int n, char s[])
-{
-    int i = 0;
-    do 
-    {
-        s[i++] = n % 10 + '0';   
-    } while ((n /= 10) > 0);     
-    s[i] = '\0';
-    reverse(s);
-} 
-
 
 pid_t fork_child(int child_num, int *fd) 
 {
     pid_t child = fork();
-    char pid[10];
     if (child == -1)
     {
         perror("fork");
@@ -41,7 +14,8 @@ pid_t fork_child(int child_num, int *fd)
     }
     else if (child == 0) 
     {
-        convert_to_str(getpid(), pid);
+        int child_pid = getpid();
+        void *pid = &child_pid;
         close(fd[0]);
         write(fd[1], pid, sizeof(pid));
         exit(0);
@@ -51,11 +25,14 @@ pid_t fork_child(int child_num, int *fd)
 
 void wait_for_childs(int *fd)
 {
+    close(fd[1]);
+    
     int stat_val;
-    char pid[10];
-    read(fd[0], pid, 10);
+    void *pid;
+    read(fd[0], pid, sizeof(pid));
+    
     pid_t child = wait(&stat_val);
-    printf("Child %d wrote %s\n", child, pid);
+    printf("Child %d wrote %d\n", child, *(int *)(pid));
     if (WIFEXITED(stat_val))
             printf("Child=%d completed normally with code=%d.\n", child, WEXITSTATUS(stat_val));
     else if (WIFSIGNALED(stat_val))
@@ -77,7 +54,6 @@ int main()
     pid_t child_2 = fork_child(2, fd);
 
     printf("parent: pid=%d, group=%d, child_1=%d, child_2=%d\n", getpid(), getpgrp(), child_1, child_2);
-    close(fd[1]);
     wait_for_childs(fd);
     wait_for_childs(fd);
     
--- Lab2/prog5.c
+++ Lab2/prog5.c
@@ -3,55 +3,25 @@
 #include <unistd.h>
 #include <sys/types.h> 
 #include <sys/wait.h>
-#include <string.h>
 
 int flag = 0;
-int fd[2];
-
-void reverse(char s[])
-{
-    int i, j;
-    char c;
-
-    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) 
-    {
-        c = s[i];
-        s[i] = s[j];
-        s[j] = c;
-    }
-}
-
-void convert_to_str(int n, char s[])
-{
-    int i = 0;
-    do 
-    {
-        s[i++] = n % 10 + '0';   
-    } while ((n /= 10) > 0);     
-    s[i] = '\0';
-    reverse(s);
-} 
 
 void catch_signal(int sig_numb)
 {
     printf("\nSignal Cntrl + C\n");
-    if (flag)
-        flag = 0;
-    else 
-        flag = 1;
+    flag = 1;
 }
 
-void wait_signal(char *operation)
+void wait_signal()
 {
-    printf("Press Ctrl + C to %s.\n", operation);
+    printf("Press Ctrl + C to write.\n");
 	signal(SIGINT, catch_signal);
-	sleep(5);
+    sleep(3);
 }
 
-pid_t fork_child(int child_num) 
+pid_t fork_child(int child_num, int *fd) 
 {
     pid_t child = fork();
-    char pid[10];
     if (child == -1)
     {
         perror("fork");
@@ -59,7 +29,8 @@ pid_t fork_child(int child_num)
     }
     else if (child == 0) 
     {
-        convert_to_str(getpid(), pid);
+        int child_pid = getpid();
+        void *pid = &child_pid;
         close(fd[0]);
         write(fd[1], pid, sizeof(pid));
         exit(0);
@@ -67,54 +38,41 @@ pid_t fork_child(int child_num)
     return child;
 }
 
-void wait_for_childs(pid_t child, int stat_val)
+void wait_for_childs(int *fd)
 {
-    char pid[10];
-    read(fd[0], pid, 10);
-    printf("Child %d wrote %s\n", child, pid);
+    close(fd[1]);
+    printf("YES\n");
+    int stat_val;
+    void *pid;
+    read(fd[0], pid, sizeof(pid));
+    
+    pid_t child = wait(&stat_val);
+    printf("Child %d wrote %d\n", child, *(int *)(pid));
     if (WIFEXITED(stat_val))
-        printf("Child=%d completed normally with code=%d.\n", child, WEXITSTATUS(stat_val));
+            printf("Child=%d completed normally with code=%d.\n", child, WEXITSTATUS(stat_val));
     else if (WIFSIGNALED(stat_val))
         printf("Child=%d ended with a non-intercepted signal with code=%d.\n", child, WTERMSIG(stat_val));
     else if (WIFSTOPPED(stat_val))
         printf("Child=%d stopped with %d code.\n", child, WSTOPSIG(stat_val));
 }
 
-void parent_read(pid_t child_1, pid_t child_2)
-{
-    if (child_1 != 0 && child_2 != 0)
-    {
-        int stat_val_1;
-        int stat_val_2;
-        pid_t child_1 = wait(&stat_val_1);
-        pid_t child_2 = wait(&stat_val_2);
-
-        wait_signal("read");
-        if (flag)
-            exit(0);
-
-        close(fd[1]);
-        wait_for_childs(child_1, stat_val_1);
-        wait_for_childs(child_2, stat_val_2);
-    }
-}
-
 int main() 
 {
+    int fd[2];
     if (pipe(fd) == -1)
 	{
         perror("pipe");
 		exit(1);
 	}
 
-    wait_signal("write");
-    
-    if (!flag)
-        exit(0);
+    wait_signal();
 
-    pid_t child_1 = fork_child(1);
-    pid_t child_2 = fork_child(2);
+    pid_t child_1 = fork_child(1, fd);
+    pid_t child_2 = fork_child(2, fd);
+
     printf("parent: pid=%d, group=%d, child_1=%d, child_2=%d\n", getpid(), getpgrp(), child_1, child_2);
-    parent_read(child_1, child_2);
+    wait_for_childs(fd);
+    wait_for_childs(fd);
+    
     return 0;
 }
